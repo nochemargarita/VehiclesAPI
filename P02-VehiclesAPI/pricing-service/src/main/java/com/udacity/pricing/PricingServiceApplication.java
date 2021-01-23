73f0ecf3096967e0946fcb19b61b344ddb3e4f2d
Margarita Noche <nochemargarita@gmail.com>
2021-01-23 14:23:49 -0800

    Convert PricingService to a microservice

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,14 +2,16 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
-
+    //    /services/price?vehicleId=4 for testing if the service started successfuly aside from checking in Eureka server
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,52 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void getPrice() {
+		ResponseEntity<Price> response =
+				this.restTemplate
+						.getForEntity("http://localhost:" + port + "/services/price?vehicleId=4", Price.class);
+
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
+
+//	@Test not needed because it just fills price in case none is given to a vehicle.
+//	public void randomPrice() {
+//		ResponseEntity<BigDecimal> response =
+//				this.restTemplate
+//						.getForEntity("http://localhost:" + port + "/services/price?vehicleId=4", BigDecimal.class);
+//
+//		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+//	}
 
 }
